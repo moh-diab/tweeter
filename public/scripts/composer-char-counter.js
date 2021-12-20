51741edf204384b0f1edec8de030484983bc1025
moh-diab <mohdiab@gmail.com>
2021-12-19 19:32:40 -0500

    create tweets and fix navbar

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,13 +2,12 @@ $(document).ready(function() {
   $("#tweet-text").on('keyup', function() {
     const counter = $(this).next().children()[1];
 
+    counter.value = 140 - $(this).val().length;
+
     if (counter.value <= 135) {
       $(counter).css("color","red");
     } else {
-      $(counter).css("color","black");
+      $(counter).css("color","#545149");
     }
-
-    counter.value = 140 - $(this).val().length;
   });
 });
-
